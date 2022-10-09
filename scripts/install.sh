0c09602bbdd7f24e5a2a5962b8a3f1172f9ab921
Simon Waldherr <git@simon.waldherr.eu>
2022-10-09 15:30:22 +0200

    cleanup

--- scripts/install.sh
+++
@@ -1,7 +0,0 @@
-export GO111MODULE=on
-
-go install github.com/gopherjs/gopherjs
-go install github.com/gopherjs/gopherjs.github.io
-go install github.com/neelance/go-angularjs
-go install honnef.co/go/js/dom
-go install honnef.co/go/js/xhr
